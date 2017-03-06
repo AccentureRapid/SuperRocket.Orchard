8de417e8e0fe8213e363f10f15f48a2a1730e5fa
DY <david0718@qq.com>
2017-03-06 18:33:22 +0800

    Initial commit

---
+++ SuperRocket.Orchard.Application/OrchardApplicationModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Abp.AutoMapper;
+using Abp.Modules;
+
+namespace SuperRocket.Orchard
+{
+    [DependsOn(typeof(OrchardCoreModule), typeof(AbpAutoMapperModule))]
+    public class OrchardApplicationModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(mapper =>
+            {
+                //Add your custom AutoMapper mappings here...
+                //mapper.CreateMap<,>()
+            });
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ SuperRocket.Orchard.Web/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SuperRocket.Orchard.Web.Controllers
+{
+    public class AboutController : OrchardControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
