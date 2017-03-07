5b40dca3ab78586d28a465ce79c3a2d661f21ca1
david <david.bingjian.yu>
2017-03-07 22:33:33 +0800

    test service and job in about controller

--- SuperRocket.Orchard.Web/Controllers/AboutController.cs
+++ SuperRocket.Orchard.Web/Controllers/AboutController.cs
@@ -1,11 +1,19 @@
-﻿using System.Web.Mvc;
+﻿using SuperRocket.Orchard.Services;
+using System.Web.Mvc;
 
 namespace SuperRocket.Orchard.Web.Controllers
 {
     public class AboutController : OrchardControllerBase
     {
+        private readonly ITestService _testService;
+
+        public AboutController(ITestService testService)
+        {
+            _testService = testService;
+        }
         public ActionResult Index()
         {
+            _testService.Test();
             return View();
         }
 	}
