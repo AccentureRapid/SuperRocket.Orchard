a7dab97926712976b3e6d5088abd2ba9dd6c347c
david.bingjian.yu <david0718@qq.com>
2017-03-08 10:57:19 +0800

    update test job

--- SuperRocket.Orchard.Application/Job/Testjob.cs
+++ SuperRocket.Orchard.Application/Job/Testjob.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SuperRocket.Orchard.Job
@@ -12,6 +13,7 @@ namespace SuperRocket.Orchard.Job
     {
         public override void Execute(int number)
         {
+            Thread.Sleep(5000);
             Console.WriteLine("{0} Test job completed with {1} counts successfully!", DateTime.Now.ToString(), number);
         }
     }
